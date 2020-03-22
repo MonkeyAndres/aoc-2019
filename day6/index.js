b4ff953fcfa1d8172ab312e5178459a48833ab2a
Andres Martin <monkeyandres@protonmail.com>
2020-03-22 21:58:28 +0100

    Refactor day 6

--- day6/index.js
+++ day6/index.js
@@ -1,5 +1,7 @@
 const F = require('../fp-utils')
 
+// PART 1
+
 const formatOrbits = (store, item) => {
   const _store = { ...store }
   const [parent, child] = item.split(')')
@@ -32,6 +34,14 @@ const recursiveCountOrbits = (store, center = 'COM', depth = 1) => {
   )
 }
 
+const part1 = F.pipe(
+  F.split('\n'),
+  F.reduce(formatOrbits, {}),
+  recursiveCountOrbits,
+)
+
+// PART 2
+
 const pathToCOM = (orbitMap, searchingOrbit) => {
   let parent = orbitMap[searchingOrbit]
   const acc = [parent]
@@ -44,11 +54,19 @@ const pathToCOM = (orbitMap, searchingOrbit) => {
   return acc
 }
 
-const part1 = F.pipe(
-  F.split('\n'),
-  F.reduce(formatOrbits, {}),
-  recursiveCountOrbits,
-)
+const removeRepeated = (list1, list2) => {
+  const filteredList1 = list1.filter(item => !list2.includes(item))
+  const filteredList2 = list2.filter(item => !list1.includes(item))
+
+  return [...filteredList1, ...filteredList2]
+}
+
+const countJumpsBetweenOrbits = (orbitA, orbitB) => orbitMap => {
+  const orbitAPath = pathToCOM(orbitMap, orbitA)
+  const orbitBPath = pathToCOM(orbitMap, orbitB)
+
+  return removeRepeated(orbitAPath, orbitBPath).length
+}
 
 const part2 = F.pipe(
   F.split('\n'),
@@ -56,15 +74,7 @@ const part2 = F.pipe(
     const [parent, child] = item.split(')')
     return { ...store, [child]: parent }
   }, {}),
-  orbitMap => {
-    const youPath = pathToCOM(orbitMap, 'YOU')
-    const santaPath = pathToCOM(orbitMap, 'SAN')
-
-    const filteredYou = youPath.filter(item => !santaPath.includes(item))
-    const filteredSan = santaPath.filter(item => !youPath.includes(item))
-
-    return filteredYou.length + filteredSan.length
-  },
+  countJumpsBetweenOrbits('YOU', 'SAN'),
 )
 
 module.exports = { part1, part2 }
