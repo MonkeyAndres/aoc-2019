cb8810f8f4edfcff0fb18fc939fe5c546502027c
Andres Martin <monkeyandres@protonmail.com>
2020-03-22 21:47:23 +0100

    Day 6 part 2

--- day6/index.js
+++ day6/index.js
@@ -32,10 +32,39 @@ const recursiveCountOrbits = (store, center = 'COM', depth = 1) => {
   )
 }
 
+const pathToCOM = (orbitMap, searchingOrbit) => {
+  let parent = orbitMap[searchingOrbit]
+  const acc = [parent]
+
+  while (parent !== 'COM') {
+    parent = orbitMap[parent]
+    acc.push(parent)
+  }
+
+  return acc
+}
+
 const part1 = F.pipe(
   F.split('\n'),
   F.reduce(formatOrbits, {}),
   recursiveCountOrbits,
 )
 
-module.exports = { part1 }
+const part2 = F.pipe(
+  F.split('\n'),
+  F.reduce((store, item) => {
+    const [parent, child] = item.split(')')
+    return { ...store, [child]: parent }
+  }, {}),
+  orbitMap => {
+    const youPath = pathToCOM(orbitMap, 'YOU')
+    const santaPath = pathToCOM(orbitMap, 'SAN')
+
+    const filteredYou = youPath.filter(item => !santaPath.includes(item))
+    const filteredSan = santaPath.filter(item => !youPath.includes(item))
+
+    return filteredYou.length + filteredSan.length
+  },
+)
+
+module.exports = { part1, part2 }
--- day6/index.test.js
+++ day6/index.test.js
@@ -1,6 +1,6 @@
 const { testDay } = require('../testing-utils')
 const { readInput } = require('../utils')
-const { part1 } = require('./index')
+const { part1, part2 } = require('./index')
 
 const DAY = 6
 const finalInput = readInput(DAY)
@@ -31,6 +31,28 @@ K)L`,
         output: 42,
       },
       { input: finalInput, output: 110190 },
+    ],
+  },
+  {
+    fn: part2,
+    io: [
+      {
+        input: `COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN`,
+        output: 4,
+      },
+      { input: finalInput, output: 343 },
     ],
   },
 ])
